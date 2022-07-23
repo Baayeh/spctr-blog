ad95cefd399fae4206a83bb6a8ae3d9b475af335
Kwasi Baayeh <kwasiantwibaayeh@gmail.com>
2022-07-23 14:12:22 +0000

    Redirecting user to the home screen after dele

--- src/BlogDetails..js
+++ src/BlogDetails..js
@@ -1,15 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useGet from './useGet';
 import axios from "axios";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isLoading } = useGet('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
 
     const handleClick = () => {
         axios.delete('http://localhost:8000/blogs/' + id)
             .then(() => {
-                alert('Blog deleted');
+                history.push('/');
             })
     }
 
